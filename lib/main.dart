8f50f1f52582d2c2c446db434d0dea4f726e7906
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-24 10:41:05 +0530

    image error

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,9 +7,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title});
 
   final String title;
 
@@ -30,45 +31,34 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-// ChoiceChips represent a single choice from a set.
 class _MyHomePageState extends State<MyHomePage> {
+  late Widget _image;
+
+  List<Widget> _list = []; // Initialize the list with empty values
+
+  @override
+  void initState() {
+    super.initState();
+    _image = Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Image.asset(
+        'assets/imgs/image.jpg',
+        height: 100,
+        width: 100,
+        fit: BoxFit.cover,
+      ),
+    );
+
+    _list = List.filled(8, _image); // Fill the list with the initialized image
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          const SliverAppBar(
-            //if you want your appbar to stay there use
-            //pinned as true
-            //when scrolling it will also
-            // make flexible space disappear
-            pinned: true,
-            //if we want the appbar to comeback immediately when we scroll back
-            //we use floating as true
-            // floating: true,
-            snap: false,
-            title: Text(
-              'Sliver App Bar',
-              style: TextStyle(color: Colors.white),
-            ),
-            expandedHeight: 150.0,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text('Extra Space'),
-              centerTitle: true,
-            ),
-            backgroundColor: Colors.blue,
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                const Text(
-                  'Sliver',
-                  style: TextStyle(fontSize: 600),
-                )
-              ],
-            ),
-          )
-        ],
+      body: Center(
+        child: Wrap(
+          children: _list,
+        ),
       ),
     );
   }
