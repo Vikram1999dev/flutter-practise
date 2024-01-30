3a6fccd22e39f11eecd50f302889fc9f021a1aa3
Vikram Dungarwal <vikramdungarwal10@gmail.com>
2024-01-30 11:48:07 +0530

    added code for range slider

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Show Time Picker'),
+      home: const MyHomePage(title: 'Range Slider'),
     );
   }
 }
@@ -33,34 +33,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String title = '';
-  String firstPage = 'First Page';
-  String secondPage = 'Second Page';
+  RangeValues values = const RangeValues(0.0, 0.5);
 
   @override
   Widget build(BuildContext context) {
+    RangeLabels labels =
+        RangeLabels(values.start.toString(), values.end.toString());
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          title,
+          widget.title,
           style: const TextStyle(color: Colors.white),
         ),
-        actions: [
-          PopupMenuButton(
-            itemBuilder: ((context) => [
-                  PopupMenuItem(value: firstPage, child: Text(firstPage)),
-                  PopupMenuItem(value: secondPage, child: Text(secondPage))
-                ]),
-            onSelected: (String newValue) {
-              setState(() {
-                title = newValue;
-              });
-            },
-          )
-        ],
-        backgroundColor: Colors.blue,
+        backgroundColor: const Color.fromARGB(255, 47, 105, 191),
       ),
-      body: const Center(),
+      body: Center(
+        child: RangeSlider(
+          values: values,
+          divisions: 10,
+          labels: labels,
+          onChanged: (newValues) {
+            setState(() {
+              values = newValues;
+            });
+          },
+        ),
+      ),
     );
   }
 }
